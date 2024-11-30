2895dfd18fe15a3fa45840f38ccc8b971ae057b5
MohammadUsmanAsegaf <usmanasegaf7@gmail.com>
2024-11-30 18:58:43 +0700

    initialization

---
+++ README.md
@@ -0,0 +1,3 @@
+# getx_blockchain_practice_project_1
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/getx_blockchain_practice_project_1/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.getx_blockchain_practice_project_1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
