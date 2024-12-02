48e2a8f9bdca95ecbc92a3b645434915c1841ab6
MohammadUsmanAsegaf <usmanasegaf7@gmail.com>
2024-12-02 19:17:04 +0700

    11.02

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -5,6 +5,19 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: _appBar(context),
+    );
+  }
+
+  PreferredSizeWidget _appBar(BuildContext context) {
+    return AppBar(
+      title: const CircleAvatar(
+        backgroundImage: NetworkImage(
+          "https://www.gravatar.com/avatar/2c7d99fe281ecd3bcd65ab915bac6dd5?s=250",
+        ),
+      ),
+      actions: [IconButton(onPressed: () {}, icon: Icon(Icons.add))],
+    );
   }
 }
