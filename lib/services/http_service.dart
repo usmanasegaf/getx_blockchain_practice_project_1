519fba3e58ed7099e7982cdf9aed49d17479e40e
MohammadUsmanAsegaf <usmanasegaf7@gmail.com>
2024-12-03 19:17:19 +0700

    19.48

---
+++ lib/services/http_service.dart
@@ -0,0 +1,19 @@
+import 'package:dio/dio.dart';
+import 'package:getx_blockchain_practice_project_1/consts.dart';
+
+class HttpService {
+  final Dio _dio = Dio();
+
+  HttpService() {
+    _configureDio();
+  }
+
+  void _configureDio() {
+    _dio.options = BaseOptions(
+      baseUrl: "https://api.cryptorank.io/v1/",
+      queryParameters: {
+        "api_key": CRYPTO_RANK_API_KEY,
+      },
+    );
+  }
+}
