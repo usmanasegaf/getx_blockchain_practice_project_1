5143500ba63cfb399654f50c6316ce447181f63c
MohammadUsmanAsegaf <usmanasegaf7@gmail.com>
2024-12-05 22:38:10 +0700

    30.03

--- lib/widgets/add_assets_dialog.dart
+++ lib/widgets/add_assets_dialog.dart
@@ -1,30 +1,51 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class AddAssetsDialogController extends GetxController {
+  RxBool loading = false.obs;
+}
 
 class AddAssetsDialog extends StatelessWidget {
-  const AddAssetsDialog({super.key});
+  final controller = Get.put(
+    AddAssetsDialogController(),
+  );
+
+  AddAssetsDialog({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Material(
-        child: Container(
-          height: MediaQuery.sizeOf(context).height * 0.40,
-          width: MediaQuery.sizeOf(context).width * 0.80,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(
-              15,
+    return Obx(
+      () => Center(
+        child: Material(
+          child: Container(
+            height: MediaQuery.sizeOf(context).height * 0.40,
+            width: MediaQuery.sizeOf(context).width * 0.80,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(
+                15,
+              ),
+              color: Colors.white,
             ),
-            color: Colors.white,
+            child: _buildUI(),
           ),
-          child: _buildUI(),
         ),
       ),
     );
   }
 
   Widget _buildUI() {
-    return Column(
-      children: [],
-    );
+    if (controller.loading.isTrue) {
+      return const Center(
+        child: SizedBox(
+          height: 30,
+          width: 30,
+          child: CircularProgressIndicator(),
+        ),
+      );
+    } else {
+      return Column(
+        children: [],
+      );
+    }
   }
 }
